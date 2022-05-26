e15bdb6e61b941f79c1096207c813f650ffe0c52
juversonlessa@gmail.com <juversonlessa@gmail.com>
2022-05-26 12:06:16 -0300

    correcao de erros

--- main.js
+++ main.js
@@ -4,21 +4,52 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verififcar se a seção passou da linha
+  // quais dados vou precisar
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limite da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add("scroll")
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove("scroll")
+    navigation.classList.remove('scroll')
   }
 }
 
 function showBackToTopButtonOnScroll() {
   if (scrollY > 550) {
-    backToTopButton.classList.add("show")
+    backToTopButton.classList.add('show')
   } else {
-    backToTopButton.classList.remove("show")
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -33,7 +64,7 @@ function closeMenu() {
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 700,
+  duration: 700
 }).reveal(`
 #home, 
 #home img, 
@@ -43,4 +74,4 @@ ScrollReveal({
 #services .card
 #about,
 #about header
-#about .content`);
+#about .content`)
